3fe232794c5cc8bee7aeb1f1ddb36b49253957de
abhisheksonkar20 <sonkar.abhishek45@gmail.com>
2020-11-19 01:33:50 +0530

    updated code and added components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Name from './Name';
+import Description from './Description';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <Name data-ns-test="project-name" />
+      <Description data-ns-test="project-description" />
+    </div>
   )
 }
 
---
+++ src/components/Name.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Name() {
+    return (
+        <React.Fragment>
+            <h1>Abhishek's E-commerce</h1>
+        </React.Fragment>
+    );
+}
