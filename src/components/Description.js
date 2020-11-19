2bd712d7e41385c1b83c78c1a127942609fa3d84
abhisheksonkar20 <sonkar.abhishek45@gmail.com>
2020-11-19 12:40:30 +0530

    correct_code 1

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,9 +1,7 @@
 import React from "react";
 export default function Description() {
     return (
-        <React.Fragment>
-            <p>This will be my e-commerce website, which I will develop when I'll learn react and 
-                other full-stack stuffs completely.</p>
-        </React.Fragment>
+    <p>This will be my e-commerce website, which I will develop when I'll learn react and 
+    other full-stack stuffs completely.</p>
     );
 }
--- src/components/Name.js
+++ src/components/Name.js
@@ -1,8 +1,6 @@
 import React from "react";
 export default function Name() {
     return (
-        <React.Fragment>
-            <h1>Abhishek's E-commerce</h1>
-        </React.Fragment>
+    <h1>Abhishek's E-commerce</h1>
     );
 }
