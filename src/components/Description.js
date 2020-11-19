91d324385a50e39b439b8107422396b8bc27035f
abhisheksonkar20 <sonkar.abhishek45@gmail.com>
2020-11-19 12:52:40 +0530

    corrected_code 2

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,7 +1,7 @@
 import React from "react";
 export default function Description() {
     return (
-    <p>This will be my e-commerce website, which I will develop when I'll learn react and 
+    <p data-ns-test="project-description">This will be my e-commerce website, which I will develop when I'll learn react and 
     other full-stack stuffs completely.</p>
     );
 }
--- src/components/Name.js
+++ src/components/Name.js
@@ -1,6 +1,6 @@
 import React from "react";
 export default function Name() {
     return (
-    <h1>Abhishek's E-commerce</h1>
+    <h1 data-ns-test="project-name">Abhishek's E-commerce</h1>
     );
 }
